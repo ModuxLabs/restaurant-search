77d9037b5b793c94fca2926c3cd9def3fbdf020f
ModuxLabs <ianlancaster@Ians-MacBook-Pro.local>
2020-11-17 15:51:19 -0700

    Updates

---
+++ precache-manifest.d838ce9669f4c058fbc169d337fa78b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ec36fd030d0dc088a37c58f62792f8",
+    "url": "/restaurant-search/index.html"
+  },
+  {
+    "revision": "fec2d1f78ba91395da98",
+    "url": "/restaurant-search/static/css/2.af317cf5.chunk.css"
+  },
+  {
+    "revision": "eb62060acada794fa442",
+    "url": "/restaurant-search/static/css/main.bdf8731b.chunk.css"
+  },
+  {
+    "revision": "fec2d1f78ba91395da98",
+    "url": "/restaurant-search/static/js/2.98fc6de0.chunk.js"
+  },
+  {
+    "revision": "6504b607ed0a3dd58da92de751a12b3d",
+    "url": "/restaurant-search/static/js/2.98fc6de0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb62060acada794fa442",
+    "url": "/restaurant-search/static/js/main.176c5ce1.chunk.js"
+  },
+  {
+    "revision": "3cc5a14314eb51597981",
+    "url": "/restaurant-search/static/js/runtime-main.f5fc3f32.js"
+  }
+]);
